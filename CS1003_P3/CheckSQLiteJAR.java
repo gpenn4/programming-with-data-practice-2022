30d3c8668d60e623fc4bc449c0d068e5e0aa40b9
Georgia Pennington <101330572+gpenn4@users.noreply.github.com>
2024-01-18 13:03:18 +0000

    Delete directory
    Signed-off-by: Georgia Pennington <101330572+gpenn4@users.noreply.github.com>

--- CS1003_P3/CheckSQLiteJAR.java
+++
@@ -1,7 +0,0 @@
-public class CheckSQLiteJAR {
-    public static void main(String[] args) throws ClassNotFoundException{
-        Class c = Class.forName("org.sqlite.JDBC");
-        System.out.println("loaded the class" + c.getName() + " correctly."); 
-    }
-    
-}
